6cd4e7ba56607887e170f99335bb93ef06f70abb
fasnaks001 <unt24cs060@uec.ac.in>
2025-03-27 14:02:21 +0530

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
-void fibonocci(int);
+void fibonacci(int);
 void main()
 {
   int num;
   printf("Enter the number of elements :");
   scanf("%d",&num);
-  fibonocci(num);
+  fibonacci(num);
 }
-void fibonocci(int num)
+void fibonacci(int num)
 {
 int n1=0,n2=1,n3,i;
 for(i=2;i<num;i++)
